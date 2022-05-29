2491f294d3759d759e9cc6ec9a64b90ba6c40a5c
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-29 14:56:32 -0500

    solve calculateMode challenge in JS and create unit tests in the spec file

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,24 @@
-const calculateMode = () => {
+exports.calculateMode = (argList) => {
+  // initialize your variables (UNIQUE_ITEMS, COUNT, RESULT)
+  let uniqueItems = set(argList);
+  let count = [];
+  let result = [];
+  // find the count of each unique item in ARG_LIST and add it to COUNT
+  for (let item of uniqueItems) {
+    count.push(argList.filter((e) => e === item).length) }
+  // add the most frequent items to RESULT
+  for (let index in count) {
+    if (count[Number(index)] === Math.max(...count)) {
+      result.push(uniqueItems[Number(index)]); }}
+  return result;
+};
 
+function set(arg1) {
+  let myArr = [];
+  for (let item of arg1) {
+    if (!myArr.includes(item)) {
+      myArr.push(item);
+    }
+  }
+  return myArr;
 }
--- js/calculateModeSpec.js
+++ js/calculateModeSpec.js
@@ -1,1 +1,8 @@
-// Write your own unit tests here!
+var cm = require('./calculateMode')
+
+console.log(JSON.stringify(cm.calculateMode([1,2,3,3])) === JSON.stringify([3]))            
+console.log(JSON.stringify(cm.calculateMode([4.5, 0, 0])) === JSON.stringify([0]))           
+console.log(JSON.stringify(cm.calculateMode([1.5, -1, 1, 1.5])) === JSON.stringify([1.5]))   
+console.log(JSON.stringify(cm.calculateMode([1,1,2,2])) === JSON.stringify([1,2]))           
+console.log(JSON.stringify(cm.calculateMode([1,2,3])) === JSON.stringify([1,2,3]))           
+console.log(JSON.stringify(cm.calculateMode(["who", "what", "where", "who"])) === JSON.stringify(["who"]))
