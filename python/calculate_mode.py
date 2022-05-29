6b2d593841a4ae6d2a5fb3bdfe8a5f5cc9076459
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-29 13:21:30 -0500

    solve the calculate_mode challenge in python

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,13 @@
-def calculate_mode():
-    pass
+def calculate_mode(arg_list):
+    # initialize your variables (UNIQUE_ITEMS, COUNT, RESULT)
+    unique_items = list(set(arg_list))
+    count = [] 
+    result = []
+    # find the count of each unique item in ARG_LIST and add it to COUNT
+    for item in unique_items:
+        count.append(arg_list.count(item))
+    # add the most frequent items to RESULT
+    for x in range(len(count)):
+        if (count[x] == max(count)):
+            result.append(unique_items[x])
+    return result
